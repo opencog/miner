c4dfbc89fd3342f9aced7279a6bca9e090e6ca4c
a-lin <a-lin@a-lin-desktop>
2009-03-13 16:44:48 +0200

    - moved opencog/moses to opencog/learning/moses - added -Wno-deprecated in compile flag to avoid multiple warning of deprecated hash_map - fixed warning in combo/variable_unifier.cc

--- opencog/moses/moses/types.cc
+++
@@ -1,9 +0,0 @@
-#include "moses/types.h"
-
-namespace moses {
-
-  const tree_score worst_possible_score=
-  std::make_pair(-(std::numeric_limits<score_t>::max()-score_t(1)),
-		 -(std::numeric_limits<complexity_t>::max()-complexity_t(1)));
-
-} //~namespace moses
